3ae9c461d4fee3cd924693399a10c67f6f242352
pwy <974834857@qq.com>
2018-08-15 10:17:08 +0800

    Changes to be committed: 	new file:   App.config 	new file:   Form1.Designer.cs 	new file:   Form1.cs 	new file:   Form1.resx 	new file:   Program.cs 	new file:   Properties/AssemblyInfo.cs 	new file:   Properties/Resources.Designer.cs 	new file:   Properties/Resources.resx 	new file:   Properties/Settings.Designer.cs 	new file:   Properties/Settings.settings 	new file:   WindowsFormsApplication6.csproj 	new file:   bin/Debug/WindowsFormsApplication6.exe 	new file:   bin/Debug/WindowsFormsApplication6.exe.config 	new file:   bin/Debug/WindowsFormsApplication6.pdb 	new file:   bin/Debug/WindowsFormsApplication6.vshost.exe 	new file:   bin/Debug/WindowsFormsApplication6.vshost.exe.config 	new file:   bin/Debug/WindowsFormsApplication6.vshost.exe.manifest 	new file:   obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   obj/Debug/WindowsFormsApplication6.Form1.resources 	new file:   obj/Debug/WindowsFormsApplication6.Properties.Resources.resources 	new file:   obj/Debug/WindowsFormsApplication6.csproj.FileListAbsolute.txt 	new file:   obj/Debug/WindowsFormsApplication6.csproj.GenerateResource.Cache 	new file:   obj/Debug/WindowsFormsApplication6.csprojResolveAssemblyReference.cache 	new file:   obj/Debug/WindowsFormsApplication6.exe 	new file:   obj/Debug/WindowsFormsApplication6.pdb

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(0, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(965, 71);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(0, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(965, 71);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "文本";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "单词\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(103, 276);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "f1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 56);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "打字！！";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(103, 411);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "a";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 461);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        string[] line;
+        int k = 0;
+        string ms="1";
+        string fname="m";
+        Random r = new Random();
+        public void Read(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Console.WriteLine(line.ToString());
+              //  label1.Text = line;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            int i=s1.Length - 1;
+            int j = s2.Length - 1;
+          
+
+            if (i<= j)
+            {
+               // MessageBox.Show("" + i + "," + j);
+                  if (s1[i] != s2[i])
+                {
+                    textBox1.Select(s1.Length - 1, 1);
+
+                }
+               
+            }
+            else
+            {
+
+                k++;
+                if (k <= line.Length - 1)
+                {
+                    textBox1.Text = " ";
+                    if (ms == "1")
+                    {
+                        textBox2.Text = line[k];
+                    }
+                    else
+                    {
+                        k = r.Next(0, line.Length);
+                        string[] s;
+                        s = line[k].Split('-');
+                        while(!s[0].Contains(textBox4.Text))
+                        {
+                            k = r.Next(0, line.Length);
+                            s = line[k].Split('-');
+                        }
+                        textBox2.Text = s[1] + s[0] + s[2];
+                        label1.Text = s[4].Replace('！', ' ');
+                    }
+                }
+                else
+                {
+                    k = 0;
+                }
+               
+            }
+          
+            
+  
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ms = "1";
+            fname = textBox3.Text;
+            string path = @"c:\" + fname + ".txt";
+            //Read(path);
+
+
+            if (File.Exists(path))
+            {
+                line = File.ReadAllLines(path, Encoding.Default);
+
+                if (ms == "1")
+                {
+                    textBox2.Text = line[0];
+                }
+                else
+                {
+                    k = r.Next(0, line.Length);
+                    string[] s;
+                    s = line[0].Split('-');
+
+                    textBox2.Text = s[1] + s[0] + s[2] ;
+                    label1.Text = s[4].Replace('！', ' ');
+                }
+
+            }
+            else
+            {
+                textBox2.Text = "不存在！";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ms = "2";
+            fname = textBox3.Text;
+            string path = @"c:\" + fname + ".txt";
+            //Read(path);
+
+
+            if (File.Exists(path))
+            {
+                line = File.ReadAllLines(path, Encoding.Default);
+
+                if (ms == "1")
+                {
+                    textBox2.Text = line[0];
+                }
+                else
+                {
+                    k = r.Next(0, line.Length);
+                    string[] s;
+                    s = line[0].Split('-');
+                    if (s.Length >= 8)
+                    {
+                        textBox2.Text = s[1] + s[0] + s[2] ;
+                        label1.Text = s[4].Replace('！', ' ');
+                    }
+                    else
+                    {
+                        textBox2.Text = "错误";
+                    }
+
+                   
+                   
+                    
+                }
+
+            }
+            else
+            {
+                textBox2.Text = "不存在！";
+            }
+        }
+
+     
+    }
+}
